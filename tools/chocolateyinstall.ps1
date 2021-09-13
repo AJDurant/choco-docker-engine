7c17612cd2d6e5925120cc37a9afe04427bddcab
Andy Durant <adurant@rapitasystems.com>
2021-09-13 17:11:05 +0100

    replace last Get-Service call

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -34,7 +34,7 @@ Function CheckServicePath ($ServiceEXE,$FolderToCheck)
 
 $DockerServiceInstanceExistsAndIsOurs = CheckServicePath 'dockerd.exe' "$toolsDir"
 
-If ((!$DockerServiceInstanceExistsAndIsOurs) -AND ([bool](Get-Service docker -ErrorAction SilentlyContinue)))
+If ((!$DockerServiceInstanceExistsAndIsOurs) -AND (sc query docker | Select-String 'SERVICE_NAME: docker' -Quiet))
 {
   $ExistingDockerInstancePath = Get-ItemProperty hklm:\system\currentcontrolset\services\* | Where-Object {($_.ImagePath -ilike '*dockerd.exe*')} | Select-Object -expand ImagePath
   Throw "You have requested that the docker service be installed, but this system appears to have an instance of an docker service configured for another folder ($ExistingDockerInstancePath). You will need to remove that instance of Docker to use the one that comes with this package."
