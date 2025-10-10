422edf1155f3181f5e43e07452612f538d22453b
Andy Durant <andy@ajdurant.co.uk>
2025-10-10 11:21:32 +0100

    use -SimpleMatch as we don't need regex matching
    fix #17

--- tools/helper.ps1
+++ tools/helper.ps1
@@ -2,47 +2,39 @@
 $EditionId = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name 'EditionID').EditionId
 $RunningOnNano = $False
 If ($EditionId -ilike '*Nano*') {
-  $RunningOnNano = $True
+    $RunningOnNano = $True
 }
 
-Function Test-ServicePath ($ServiceEXE, $FolderToCheck)
-{
-  if ($RunningOnNano) {
-    #The NANO TP5 Compatible Way:
-    Return ([bool](@(wmic service | Where-Object {$_ -ilike "*$ServiceEXE*"}) -ilike "*$FolderToCheck*"))
-  }
-  Else
-  {
-    #The modern way:
-    Return ([bool]((Get-WmiObject win32_service | Where-Object {$_.PathName -ilike "*$ServiceEXE*"} | Select-Object -expand PathName) -ilike "*$FolderToCheck*"))
-  }
+Function Test-ServicePath ($ServiceEXE, $FolderToCheck) {
+    if ($RunningOnNano) {
+        #The NANO TP5 Compatible Way:
+        Return ([bool](@(wmic service | Where-Object { $_ -ilike "*$ServiceEXE*" }) -ilike "*$FolderToCheck*"))
+    }
+    Else {
+        #The modern way:
+        Return ([bool]((Get-WmiObject win32_service | Where-Object { $_.PathName -ilike "*$ServiceEXE*" } | Select-Object -expand PathName) -ilike "*$FolderToCheck*"))
+    }
 }
 
-Function Test-OurDockerd
-{
-  return (Test-ServicePath 'dockerd.exe' "$env:ProgramFiles") -Or (Test-ServicePath 'dockerd.exe' "$toolsDir")
+Function Test-OurDockerd {
+    return (Test-ServicePath 'dockerd.exe' "$env:ProgramFiles") -Or (Test-ServicePath 'dockerd.exe' "$toolsDir")
 }
 
-Function Test-OurOldDockerd
-{
-  return Test-ServicePath 'dockerd.exe' "$toolsDir"
+Function Test-OurOldDockerd {
+    return Test-ServicePath 'dockerd.exe' "$toolsDir"
 }
 
-Function Test-DockerdConflict
-{
-  If (-not (Test-OurDockerd) -AND (sc.exe query docker | Select-String 'SERVICE_NAME: docker' -Quiet))
-  {
-    $ExistingDockerInstancePath = Get-ItemProperty hklm:\system\currentcontrolset\services\* | Where-Object {($_.ImagePath -ilike '*dockerd.exe*')} | Select-Object -expand ImagePath
-    Throw "You have requested that the docker service be installed, but this system appears to have an instance of an docker service configured for another folder ($ExistingDockerInstancePath). You will need to remove that instance of Docker to use the one that comes with this package."
-  }
+Function Test-DockerdConflict {
+    If (-not (Test-OurDockerd) -AND (sc.exe query docker | Select-String 'SERVICE_NAME: docker' -SimpleMatch -Quiet)) {
+        $ExistingDockerInstancePath = Get-ItemProperty hklm:\system\currentcontrolset\services\* | Where-Object { ($_.ImagePath -ilike '*dockerd.exe*') } | Select-Object -expand ImagePath
+        Throw "You have requested that the docker service be installed, but this system appears to have an instance of an docker service configured for another folder ($ExistingDockerInstancePath). You will need to remove that instance of Docker to use the one that comes with this package."
+    }
 }
 
-Function Test-DockerdRunning
-{
-  return [bool](C:\Windows\System32\sc.exe query docker | Select-String 'RUNNING' -Quiet)
+Function Test-DockerdRunning {
+    return [bool](C:\Windows\System32\sc.exe query docker | Select-String 'RUNNING' -SimpleMatch -Quiet)
 }
 
-Function Test-DockerdStopped
-{
-  return [bool](C:\Windows\System32\sc.exe query docker | Select-String 'STOPPED' -Quiet)
+Function Test-DockerdStopped {
+    return [bool](C:\Windows\System32\sc.exe query docker | Select-String 'STOPPED' -SimpleMatch -Quiet)
 }
